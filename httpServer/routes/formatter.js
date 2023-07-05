d9bcadb8d7334f3344c2d7a9a08672214ee5ace5
cIel104 <kai104toshiki@gmail.com>
2023-07-05 16:38:07 +0900

    miz_formatの組み込み

--- httpServer/routes/formatter.js
+++ httpServer/routes/formatter.js
@@ -4,34 +4,36 @@ const { route } = require('../app');
 const path = require('path');
 const makeDir = require("make-dir");
 const fs = require('fs');
-const { execFileSync } = require('node:child_process')
+const { execFileSync, spawnSync } = require('node:child_process')
 const router = express.Router();
 const runGitCommand = require('../verifierKicker/runGitCommand')
 
 router.post('/', async function (req, res, next) {
     const directoryName = await runGitCommand(req.body.repositoryUrl)
-    let formatterCommand = '';
-    if (process.platform === 'win32') {
-        formatterCommand = path.relative(__dirname, path.join(path.dirname(__dirname), 'mizarFormatter', 'mizarformat.exe'));
-    } else {
-        formatterCommand = 'python ' + path.relative(__dirname, path.join(__dirname, 'mizarFormatter', 'mizarformat.py'));
-    }
-    console.log(__dirname)
-    formatterCommand = formatterCommand + ' ' + path.join(directoryName, 'text', req.body.fileName)
-    console.log(fs.existsSync(path.relative(__dirname, path.join(__dirname, 'mizarFormatter', 'mizarformat.py'))))
-    runFormatterCommand(formatterCommand)
-    const fileContent = fs.readFileSync(path.join(directoryName, 'text', req.body.fileName), 'utf-8');
+
+    const formatterCommand = ['./' + path.relative(__dirname, path.join(__dirname, 'mizarFormatter', 'miz_format')),
+    path.join(directoryName, 'text', req.body.fileName),
+    path.relative(__dirname, path.join(__dirname, 'mizarFormatter', 'mml.vct')),
+    req.body.userSettings]
+
     console.log(formatterCommand)
+    const result = spawnSync('./' + path.relative(__dirname, path.join(__dirname, 'mizarFormatter', 'miz_format')),
+        [path.join(directoryName, 'text', req.body.fileName),
+        path.relative(__dirname, path.join(__dirname, 'mizarFormatter', 'mml.vct')),
+        JSON.stringify(req.body.userSettings)], { shell: true });
+    console.log('stdout', result.stdout.toString())
+    console.log('stderr', result.stderr.toString())
+    let isFormatterSuccess = true;
+    if (result.stderr.toString()) {
+        isFormatterSuccess = false
+    }
+    const fileContent = fs.readFileSync(path.join(directoryName, 'text', req.body.fileName), 'utf-8');
+
     res.json({
         'fileContent': fileContent,
+        'isFormatterSuccess': isFormatterSuccess,
+        'errorLog': result.stderr.toString(),
     })
 })
-
-function runFormatterCommand(command) {
-    const parts = command.split(' ');
-    cmd = parts[0];
-    const args = parts.splice(1);
-    execFileSync(cmd, args)
-}
 
 module.exports = router;
