cee29b09e8b0c059653f334b887f05a4e5b7e37b
DESKTOP-DP2PCI0\kai <kaitoshiki104@gmail.com>
2024-05-23 14:46:17 +0900

    Httpリクエストをログファイルに保存

--- httpServer/app.ts
+++ httpServer/app.ts
@@ -4,6 +4,8 @@ import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import express from 'express';
+import fs from "node:fs"
+import * as rfs from 'rotating-file-stream';
 const verifierRouter = require('./routes/api')
 const formatterRouter = require('./routes/formatter')
 const linterRouter = require('./routes/linter')
@@ -17,7 +19,21 @@ app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Httpリクエストログの出力とファイル保存
 app.use(logger('dev'));
+const logDirectory = path.join(__dirname, './log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+const requestLogStream = rfs.createStream('requestLog', {
+  size: '10MB',
+  interval: '10d', // 期間(s:秒、m:分、h:時間、d:日)
+  compress: 'gzip', // 期間を過ぎたログの圧縮方法
+  path: logDirectory
+})
+const logFormat = ':date[clf] - :method :status :url - :response-time ms'
+app.use(logger(logFormat, {
+  stream: requestLogStream
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false, limit: '1000mb' }));
 app.use(cookieParser());
@@ -40,7 +56,7 @@ app.use(function (err: { message: any; status: any; }, req: { app: { get: (arg0:
 
   // render the error page
   res.status(err.status || 500);
-  res.json({ error: err.message})
+  res.json({ error: err.message })
   // res.render('error');
 });
 
